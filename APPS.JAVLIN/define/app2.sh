63a5d66fa1a9cb9fc3cdaac10a946a82544742d1
damia-yt <68936A@gmail.com>
2020-10-18 01:50:03 +0000

    Initial commit

---
+++ APPS.JAVLIN/define/app2.sh
@@ -0,0 +1,1 @@
+echo JavlinOS 1.0 Made By Damien S. or repl.it/@damiascripting
---
+++ main.sh
@@ -0,0 +1,6 @@
+echo Welcome To JavlinOS
+echo Type 'bash APPS.JAVLIN/[App Name]' To Launch An App
+echo Apps:
+echo 1. About
+echo 2. kitten.exe
+echo 3. FILE-EXPLORER
